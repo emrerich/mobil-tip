2d176a75c1851fadcbaabd2584f154beba528508
Mehmet Celikten <104172253+Mehmetcelikten@users.noreply.github.com>
2024-12-28 11:19:26 +0300

    Update RegistrationScreen.js
    New type

--- src/screens/RegistrationScreen/RegistrationScreen.js
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -46,7 +46,8 @@ export default function RegistrationScreen({navigation}) {
     
 
     return (
-        <View style={styles.container}>
+        <View 
+        style={styles.container}>
             <KeyboardAwareScrollView
                 style={{ flex: 1, width: '100%' }}
                 keyboardShouldPersistTaps="always">
